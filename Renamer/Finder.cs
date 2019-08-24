871ddea8738b30ada592467915cb3e1e8f28386f
Ildar <tag.ildar@gmail.com>
2019-08-24 12:53:01 +0300

    Реализовано получение файлов и папок, rename работает из Program

---
+++ Renamer/Finder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renamer
+{
+	class Finder
+	{
+		public void PrintAllDirs(string path, bool allPath = false, string divChar = "\t", string divStr = "")
+		{
+			Console.WriteLine(divStr + (allPath ?
+					path :
+					path.Substring(path.LastIndexOf("\\"))
+					));
+
+			string[] dirs = Directory.GetDirectories(path);
+			foreach (string dir in dirs)
+				PrintAllDirs(dir, allPath, divChar, divStr + divChar);
+		}
+
+		public void PrintAllFiles(string path, bool allPath = false, string divChar = "\t", string divStr = "")
+		{
+			string[] files = Directory.GetFiles(path);
+			foreach (string file in files)
+				Console.WriteLine(divStr + (allPath ?
+					file :
+					file.Replace(path + "\\", "")));
+
+			string[] dirs = Directory.GetDirectories(path);
+			foreach (string dir in dirs)
+				PrintAllFiles(dir, allPath, divChar, divStr + divChar);
+		}
+
+		public void GetAllFiles(string path, List<string> fs)
+		{
+			string[] files = Directory.GetFiles(path);
+			foreach (string file in files) fs.Add(file);
+
+			string[] dirs = Directory.GetDirectories(path);
+			foreach (string dir in dirs)
+				GetAllFiles(dir, fs);
+		}
+
+		public void Rename(List<string> fs)
+		{
+
+		}
+
+		public void Rename(string file)
+		{
+
+		}
+	}
+}
--- Renamer/Program.cs
+++ Renamer/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Renamer
@@ -10,6 +12,33 @@ namespace Renamer
 	{
 		static void Main(string[] args)
 		{
+			string path = @"D:\TEST\Animations";
+			Finder finder = new Finder();
+			List<string> files = new List<string>();
+
+			//finder.PrintAllFiles(path);
+			//finder.GetAllFiles(path, files);
+			//finder.PrintAllDirs(path, false, " ");
+
+			Console.WriteLine("\n===============================================================================================\n");
+
+			finder.GetAllFiles(path, files);
+
+
+			Regex reg = new Regex("^peace.*anim$", RegexOptions.IgnoreCase);
+			foreach (string file in files)
+			{
+				FileInfo fi = new FileInfo(file);
+				if (reg.IsMatch(fi.Name))
+				{
+					Console.WriteLine(fi.Name);
+					Console.WriteLine(fi.DirectoryName);
+					fi.MoveTo(fi.DirectoryName + "\\_" + fi.Name);
+				}
+					
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
